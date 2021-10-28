635f863e495754586010f84516f45dda1976f9bd
Jaehyung You <jaehyung.you@travelflan.com>
2021-10-28 16:32:47 +0900

    add dp problem solved3

--- src/main/java/com/programmers/medium/DPProblem.java
+++ src/main/java/com/programmers/medium/DPProblem.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class DPProblem {
@@ -26,5 +27,28 @@ public class DPProblem {
         }
 
         return Arrays.stream(triangle[triangle.length - 1]).max().getAsInt();
+    }
+
+    // https://programmers.co.kr/learn/courses/30/lessons/42897
+
+    public static int neighborHouse(int[] money) {
+        int answer = 0;
+        List<Integer> moneyList = Arrays.stream(money).boxed().collect(Collectors.toList());
+        for (int i = 0, moneyListSize = moneyList.size(); i < moneyListSize; i++) {
+            answer += Collections.max(moneyList);
+            int index = moneyList.indexOf(Collections.max(moneyList));
+            moneyList.set(index, 0);
+            if (index == moneyListSize - 1) {
+                moneyList.set(0, 0);
+                moneyList.set(index - 1, 0);
+            } else if (index == 0) {
+                moneyList.set(index + 1, 0);
+                moneyList.set(moneyListSize - 1, 0);
+            } else {
+                moneyList.set(index - 1, 0);
+                moneyList.set(index + 1, 0);
+            }
+        }
+        return answer;
     }
 }
--- src/test/java/com/programmers/medium/DPProblemTest.java
+++ src/test/java/com/programmers/medium/DPProblemTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static com.programmers.medium.DPProblem.intTriangle;
+import static com.programmers.medium.DPProblem.neighborHouse;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DPProblemTest {
@@ -26,4 +27,22 @@ class DPProblemTest {
                 Arguments.of(new int[][] {{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}}, 30)
         );
     }
+
+    @ParameterizedTest
+    @MethodSource("neighborHouseTestCases")
+    @DisplayName("DP 테스트-2")
+    void neighborHouseTest(int[] money, int expected)  {
+        Assertions.assertEquals(expected, neighborHouse(money));
+    }
+
+    static Stream<Arguments> neighborHouseTestCases() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 2, 3, 1}, 4),
+                Arguments.of(new int[]{1, 5, 1, 5, 1}, 10),
+                Arguments.of(new int[]{1000, 0, 0, 1000, 0, 0, 1000, 0, 0, 1000}, 3000),
+                Arguments.of(new int[]{1000, 1, 0, 1, 2, 1000, 0}, 2001),
+                Arguments.of(new int[]{10, 2, 2, 100, 2}, 110)
+        );
+    }
+
 }
