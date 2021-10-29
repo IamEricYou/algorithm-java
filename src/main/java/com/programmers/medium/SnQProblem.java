b10f473569804d5a5bdc21f29b956c323a2d2cfb
Jaehyung You <jaehyung.you@travelflan.com>
2021-10-29 14:30:17 +0900

    queue problem added and solved

--- src/main/java/com/programmers/medium/SnQProblem.java
+++ src/main/java/com/programmers/medium/SnQProblem.java
@@ -1,10 +1,35 @@
 package com.programmers.medium;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.stream.Collectors;
+
 public class SnQProblem {
 
     // https://programmers.co.kr/learn/courses/30/lessons/42586?language=java
     public static int[] developNewFeature(int[] progresses, int[] speeds) {
         int[] answer = {};
-        return answer;
+        List<Integer> answerList = new ArrayList<>();
+        int numberOfWork = 1;
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0, progressesLength = progresses.length; i < progressesLength; i++) {
+            int currentWorkToBeCompleted = (100 - progresses[i]) / speeds[i];
+            if ((100 - progresses[i]) % speeds[i] != 0) currentWorkToBeCompleted += 1;
+            queue.add(currentWorkToBeCompleted);
+        }
+
+        int target = queue.poll();
+        for (int q : queue) {
+            if (target >= q) {
+                numberOfWork += 1;
+            } else {
+                answerList.add(numberOfWork);
+                numberOfWork = 1;
+            }
+        }
+        answerList.add(numberOfWork);
+        return answerList.stream().mapToInt(Integer::intValue).toArray();
     }
 }
--- src/test/java/com/programmers/medium/SnQProblemTest.java
+++ src/test/java/com/programmers/medium/SnQProblemTest.java
@@ -12,8 +12,26 @@ class SnQProblemTest {
     void solutionTest() {
         int[] p = new int[]{93, 30, 55};
         int[] s = new int[]{1, 30, 5};
-        int[] r = new int[]{1, 3, 2};
+        int[] r = new int[]{2, 1};
 
         Assertions.assertArrayEquals(r, developNewFeature(p, s));
+    }
+
+    @Test
+    void solutionTest2() {
+        int[] p2 = new int[]{99, 99, 99};
+        int[] s2 = new int[]{1, 1, 1};
+        int[] r2 = new int[]{3};
+
+        Assertions.assertArrayEquals(r2, developNewFeature(p2, s2));
+    }
+
+    @Test
+    void solutionTest3() {
+        int[] p3 = new int[]{96, 99, 98, 97};
+        int[] s3 = new int[]{1, 1, 1, 1};
+        int[] r3 = new int[]{4};
+
+        Assertions.assertArrayEquals(r3, developNewFeature(p3, s3));
     }
 }
